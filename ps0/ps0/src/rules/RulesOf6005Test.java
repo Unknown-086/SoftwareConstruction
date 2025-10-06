819c346d4a90638f8585875a94a5059368f88238
UnKnowN <hadipak2013@gmail.com>
2025-10-06 22:03:25 +0500

    Implemented the mayUseCodeInAssignment method and Added new Test methods in Rulesof6005Test.java

--- ps0/ps0/src/rules/RulesOf6005Test.java
+++ ps0/ps0/src/rules/RulesOf6005Test.java
@@ -11,7 +11,7 @@ import org.junit.Test;
  * JUnit tests for RulesOf6005.
  */
 public class RulesOf6005Test {
-    
+
     /**
      * Tests the mayUseCodeInAssignment method.
      */
@@ -21,5 +21,16 @@ public class RulesOf6005Test {
                 RulesOf6005.mayUseCodeInAssignment(false, true, false, false, false));
         assertTrue("Expected true: self-written required code",
                 RulesOf6005.mayUseCodeInAssignment(true, false, true, true, true));
+    }
+
+    // My own Test Functions
+    @Test
+    public void testOwnWorkAllowed() {
+        assertTrue(RulesOf6005.mayUseCodeInAssignment(true, false, false, false, true));
+    }
+
+    @Test
+    public void testCopyWithoutCitationNotAllowed() {
+        assertFalse(RulesOf6005.mayUseCodeInAssignment(false, true, false, false, false));
     }
 }
